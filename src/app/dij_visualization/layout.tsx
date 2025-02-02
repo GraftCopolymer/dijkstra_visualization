9bd9b7bca4315e25c4e4b54c22b8c29deddbd156
GraftCopolymer <2819952726@qq.com>
2025-02-02 18:11:55 +0800

    feat: 完成页面的基本框架
    feat: 完成可拖拽的控制面板

--- src/app/dij_visualization/layout.tsx
+++ src/app/dij_visualization/layout.tsx
@@ -11,5 +11,6 @@ export default function VisualizationLayout({
 } : Readonly<{children: React.ReactNode}>){
     return <div>
         <TitleBar />
+        {children}
     </div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
 import Link from "next/link";
 import styles from "./page.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(()=>{
+    // 直接跳转到算法可视化页面
+    router.push("/dij_visualization")
+  }, [])
+
   return (
     <div className={styles.page}>
-      <Link href='/dij_visualization'>可视化演示</Link>
+      <p>加载中</p>
     </div>
   );
 }
